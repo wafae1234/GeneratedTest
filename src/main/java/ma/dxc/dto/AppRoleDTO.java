dcc4bba62623087ab09bc8a22a0a1a82d5bac672
unknown <med.benali.pro@gmail.com>
2020-05-10 09:23:21 +0200

    fixing conflict

--- src/main/java/ma/dxc/dto/AppRoleDTO.java
+++ src/main/java/ma/dxc/dto/AppRoleDTO.java
@@ -1,6 +1,5 @@
 package ma.dxc.dto;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -13,28 +12,15 @@ public class AppRoleDTO {
 	private String roleName;
 	private Collection<Permission> permissions = new ArrayList<>();
 	
-=======
-import java.util.Objects;
-
-public class AppRoleDTO {
-	private Long id;
-	private String roleName;
->>>>>>> 03c545f918a86ab8cc3fe1130c64efb73864dc5f
 	public AppRoleDTO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-<<<<<<< HEAD
 	public AppRoleDTO(Long id, String roleName, Collection<Permission> permissions) {
 		super();
 		this.id = id;
 		this.roleName = roleName;
 		this.permissions = permissions;
-=======
-	public AppRoleDTO(String roleName) {
-		super();
-		this.roleName = roleName;
->>>>>>> 03c545f918a86ab8cc3fe1130c64efb73864dc5f
 	}
 	public Long getId() {
 		return id;
@@ -48,35 +34,10 @@ public class AppRoleDTO {
 	public void setRoleName(String roleName) {
 		this.roleName = roleName;
 	}
-<<<<<<< HEAD
 	public Collection<Permission> getPermissions() {
 		return permissions;
 	}
 	public void setPermissions(Collection<Permission> permissions) {
 		this.permissions = permissions;
 	}
-=======
-	@Override
-	public String toString() {
-		return "AppRoleDTO [id=" + id + ", roleName=" + roleName + "]";
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, roleName);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		AppRoleDTO other = (AppRoleDTO) obj;
-		return Objects.equals(id, other.id) && Objects.equals(roleName, other.roleName);
-	}
-	
-	
-	
->>>>>>> 03c545f918a86ab8cc3fe1130c64efb73864dc5f
 }
