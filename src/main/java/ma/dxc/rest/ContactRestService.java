1d5a7452ff614712da310348d208779d597a2a15
unknown <med.benali.pro@gmail.com>
2020-05-10 14:52:23 +0200

    fixing authentication problem

--- src/main/java/ma/dxc/rest/ContactRestService.java
+++ src/main/java/ma/dxc/rest/ContactRestService.java
@@ -156,27 +156,7 @@ public class ContactRestService {
 		return contactOrchestration.getPageofContacts(page, size);
 	}
 	
-	@PreAuthorize("permitAll()")
-	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
-
-		try {
-			authenticationManager.authenticate(
-					new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-			);
-		}
-		catch (BadCredentialsException e) {
-			throw new Exception("Incorrect username or password", e);
-		}
-
-
-		final UserDetails userDetails = userDetailsService
-				.loadUserByUsername(authenticationRequest.getUsername());
-
-		final String jwt = jwtTokenUtil.generateToken(userDetails);
- 
-		return ResponseEntity.ok(new AuthenticationResponse(jwt));
-	}
+	
 	
 
 }
