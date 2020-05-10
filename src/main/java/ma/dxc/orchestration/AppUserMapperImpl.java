43cdfabd52e601ef60153c73418a52fcf8fe903e
unknown <med.benali.pro@gmail.com>
2020-05-10 09:27:14 +0200

    fixing conflict

--- src/main/java/ma/dxc/orchestration/AppUserMapperImpl.java
+++ src/main/java/ma/dxc/orchestration/AppUserMapperImpl.java
@@ -1,6 +1,5 @@
 package ma.dxc.orchestration;
 
-<<<<<<< HEAD
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
@@ -8,9 +7,6 @@ import org.modelmapper.TypeToken;
 import org.springframework.data.domain.Page;
 
 import java.lang.reflect.Type;
-=======
-import org.modelmapper.ModelMapper;
->>>>>>> 03c545f918a86ab8cc3fe1130c64efb73864dc5f
 
 import ma.dxc.dto.AppUserDTO;
 import ma.dxc.model.AppUser;
@@ -19,7 +15,6 @@ public class AppUserMapperImpl implements AppUserMapper {
 	ModelMapper modelMapper = new ModelMapper();
 
 	@Override
-<<<<<<< HEAD
 	public AppUserDTO toAppUserDTO(AppUser AppUser) {
 		return modelMapper.map(AppUser, AppUserDTO.class);
 	}
@@ -44,15 +39,6 @@ public class AppUserMapperImpl implements AppUserMapper {
 		Type listType = new TypeToken<Page<AppUserDTO>>(){}.getType();
 		Page<AppUserDTO> AppUserDTOspageable = modelMapper.map(appUsers,listType);
 		return AppUserDTOspageable;
-=======
-	public AppUserDTO toAppUserDTO(AppUser appUser) {
-		return modelMapper.map(appUser, AppUserDTO.class);
-	}
-
-	@Override
-	public AppUser toAppUser(AppUserDTO appUserDto) {
-		return modelMapper.map(appUserDto, AppUser.class);
->>>>>>> 03c545f918a86ab8cc3fe1130c64efb73864dc5f
 	}
 
 }
