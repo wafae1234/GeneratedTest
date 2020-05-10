17a5db59d73471cc14101f1047d70a2bd010e8eb
unknown <med.benali.pro@gmail.com>
2020-05-10 22:19:40 +0200

    fixing search role py permission

--- src/main/java/ma/dxc/service/RoleServiceImpl.java
+++ src/main/java/ma/dxc/service/RoleServiceImpl.java
@@ -1,15 +1,18 @@
 package ma.dxc.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import ma.dxc.model.AppRole;
 import ma.dxc.model.Contact;
+import ma.dxc.model.Permission;
 import ma.dxc.repository.RoleRepository;
 import ma.dxc.repository.specs.ContactSpecification;
 import ma.dxc.repository.specs.RoleSpecification;
@@ -21,6 +24,9 @@ public class RoleServiceImpl implements RoleService {
 	
 	@Autowired
 	RoleRepository roleRepository;
+	
+	@Autowired
+	PermissionServiceImpl permissionServiceImpl;
 
 	@Override
 	public List<AppRole> findAll() {
@@ -46,9 +52,21 @@ public class RoleServiceImpl implements RoleService {
 				roleRepository.findAll();
 				Pageable pageable = PageRequest.of(page, size);
 				RoleSpecification roleSpecification = new RoleSpecification();
-				if(isNumeric(mc)) {
+				 if(column.equals("permissions")){
+					List<Permission> permissions = permissionServiceImpl.search(mc, page, size, "permissionName").getContent();
+					Permission permission = permissions.get(0);
+					List<AppRole> allRoles = roleRepository.findAll();
+					List<AppRole> roles = new ArrayList<AppRole>();
+					for (AppRole role : allRoles) {
+						if(role.getPermissions().contains(permission)) {
+							roles.add(role);
+						}
+					}
+					Page<AppRole>appRolePage = new PageImpl<AppRole>(roles, pageable, roles.size());
+					return appRolePage;
+				}else if(isNumeric(mc)) {
 					roleSpecification.add(new SearchCriteria(column, mc, SearchOperation.EQUAL));
-				}else {
+				}else{
 					roleSpecification.add(new SearchCriteria(column, mc, SearchOperation.MATCH));
 				}
 				//pagination des resultats
